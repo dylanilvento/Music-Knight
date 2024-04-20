488adffb68d726b1714a6f195c3457652acc0638
Dylan Ilvento <dylanilvento@gmail.com>
2024-04-20 12:46:38 -0400

    Put PlayerScript through CSharpier and got sprite flipping working.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,7 +7,9 @@ public class PlayerScript : MonoBehaviour
 {
     //var speed: int = 5;
     public int speed = 5;
-    
+
+    bool facingLeft = true;
+
     GameObject collidedWith;
 
     public GameObject drumbeatPrefab;
@@ -20,72 +22,87 @@ public class PlayerScript : MonoBehaviour
     int fire1GUIDistance = 0;
     int fire1GUICount = 0;
 
-	Rigidbody rb;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-		rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Math.Abs(Input.GetAxis("Horizontal")) > 0)
-		{
-			
-			//Vector 3 (x, y, z)
-			transform.Translate(new Vector3(Input.GetAxis("Horizontal") * -speed * Time.deltaTime, 0, 0));
-			
-			//Texture flip
-			if (Input.GetKey("left"))
-			{
+        float move = Input.GetAxis("Horizontal");
 
-				transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
-				
-			}
-			//Texture flip
-			if (Input.GetKey("right"))
-			{
-				
-				transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
-				
-			}
-		}
-		//jump
-		//not working correctly, may be due to fire1, only when on ground
-		if (Input.GetButtonDown("Jump"))
-		{
-			//	    speed = 1;
-			if (collidedWith != null)
-			{
-				if (collidedWith.tag == "Ground")
-				{
-					rb.velocity = new Vector3(0, 3, 0);
-					return;
-				}
-			}
-		}
+        if (Math.Abs(move) > 0)
+        {
+            //Vector 3 (x, y, z)
+            transform.Translate(new Vector3(move * -speed * Time.deltaTime, 0, 0));
 
-		if (Input.GetButtonDown("Fire1"))
-		{
+            //Texture flip
+            if (move < 0 && !facingLeft)
+            {
+                FlipDirection();
+                facingLeft = true;
+            }
 
-			GameObject currDrumBeat;
+            if (move > 0 && facingLeft)
+            {
+                FlipDirection();
+                facingLeft = false;
+            }
+        }
+        //jump
+        //not working correctly, may be due to fire1, only when on ground
+        if (Input.GetButtonDown("Jump"))
+        {
+            //	    speed = 1;
+            if (collidedWith != null)
+            {
+                if (collidedWith.tag == "Ground")
+                {
+                    rb.velocity = new Vector3(0, 3, 0);
+                    return;
+                }
+            }
+        }
 
-			//facing left
-			if (transform.localScale.x > 0f)
-			{
-				//Instantiate(prefab, location, rotation)
-				currDrumBeat = Instantiate(drumbeatPrefab, new Vector3(transform.position.x + 0.6f, transform.position.y + 0.1f, 6), Quaternion.identity);
-				fire1GUIShow = true;
-			}
-			//facing right
-			else if (transform.localScale.x < 0f)
-			{
-				currDrumBeat = Instantiate(drumbeatPrefab, new Vector3(transform.position.x - 0.6f, transform.position.y + 0.1f, 6), Quaternion.identity);
-				currDrumBeat.GetComponent<DrumBeatScript>().speed *= -1;
-				fire1GUIShow = true;
-			}
-		}
-	}
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject currDrumBeat;
+
+            //facing left
+            if (transform.localScale.x > 0f)
+            {
+                //Instantiate(prefab, location, rotation)
+                currDrumBeat = Instantiate(
+                    drumbeatPrefab,
+                    new Vector3(transform.position.x + 0.6f, transform.position.y + 0.1f, 6),
+                    Quaternion.identity
+                );
+                fire1GUIShow = true;
+            }
+            //facing right
+            else if (transform.localScale.x < 0f)
+            {
+                currDrumBeat = Instantiate(
+                    drumbeatPrefab,
+                    new Vector3(transform.position.x - 0.6f, transform.position.y + 0.1f, 6),
+                    Quaternion.identity
+                );
+                currDrumBeat.GetComponent<DrumBeatScript>().speed *= -1;
+                fire1GUIShow = true;
+            }
+        }
+    }
+
+    void FlipDirection()
+    {
+        transform.localScale = new Vector3(
+            transform.localScale.x * -1f,
+            transform.localScale.y,
+            transform.localScale.z
+        );
+    }
 }
